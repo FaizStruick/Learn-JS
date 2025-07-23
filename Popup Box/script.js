60c955142c1c28381e43ee56a083e7d78e294810
Faiz-Super <faizmhmad15@gmail.com>
2025-07-23 11:15:25 +0700

    POPUP BOX

---
+++ Popup Box/script.js
@@ -0,0 +1,31 @@
+// Alert
+//alert('Hello Faiz!');
+//alert('hayuukk');
+//alert('Gaming hahay');
+
+// Prompt
+//var nama = prompt('Masukan Nama Anda : ');
+//alert(nama);
+
+// Confirm
+//var tes = confirm('Kamu yakin kah?');
+//if (tes === true) {
+ //   alert('User menekan tombol OK');
+//} else {
+//    alert('User menekan tombol CANCEL');
+// }
+
+alert('Selamat Datang');
+var lagi = true;
+
+while ( lagi ){
+    var nama = prompt('Masukan nama Anda : ');
+    alert('Halo ' + nama);
+
+    lagi = confirm('Mau coba lagi?');
+}
+
+alert('Terima Kasih!');
+
+
+
