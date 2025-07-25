9c753802ad2d3d956844a6e54166ece1fa2ebcf6
Faiz-Super <faizmhmad15@gmail.com>
2025-07-25 22:28:15 +0700

    Pengkondisian if-else if-else

--- Pengkondisian/angkot3.js
+++ Pengkondisian/angkot3.js
@@ -12,15 +12,15 @@
 
 var jmlAngkot = 10;
 var angkotBeroprasi = 6;
-var angkotLembur = 8;
+
 
     for ( var noAngkot = 1; noAngkot <= jmlAngkot; noAngkot++){
 
-        if ( noAngkot <= angkotBeroprasi){
-            console.log('Angkot No. ' + noAngkot + ' beroprasi dengan baik.');
-        } else if ( noAngkot == angkotLembur){
-            console.log('Angkot No. ' + noAngkot + ' sedang lembur');
+        if ( noAngkot <= 6 && noAngkot != 5 ) {
+            console.log('Angkot No.' + noAngkot + ' sedang beroprasi dengan baik');
+        } else if ( noAngkot == 8 || noAngkot == 10 || noAngkot == 5 ){
+            console.log('Angkot No.' + noAngkot + ' sedang lembur.');
         } else {
-            console.log('Angkot No. ' + noAngkot + ' tidak beroprasi dengan baik');
+            console.log('Angkot No.' + noAngkot + ' tidak beroprasi dengan baik.');
         }
     }
