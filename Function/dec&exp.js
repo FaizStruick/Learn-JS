c0811720bbd0164d5652703e22694e14538696a9
Faiz-Super <faizmhmad15@gmail.com>
2025-08-02 11:56:43 +0700

    Dec & Exp

---
+++ Function/dec&exp.js
@@ -0,0 +1,11 @@
+// namaSaya('Faiz');
+
+// function namaSaya(nama){
+//     alert('Hallo' + nama);
+// }
+
+    var tampilkanPesan = function(nama) {
+        alert('Halo ' + nama);
+    }
+
+    tampilkanPesan('Faiz')
