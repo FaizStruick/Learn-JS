18ff2186bce8412b1d9d041ad28067acd69c1f2b
Faiz-Super <faizmhmad15@gmail.com>
2025-08-22 04:49:24 +0700

    Project function & Array

---
+++ Project func & arr/project.js
@@ -0,0 +1,11 @@
+var penumpang = [];
+var tambahPenumpang = function(namaPenumpang, penumpang) {
+
+    if (penumpang == 0){
+        // Menambah penumpang
+        penumpang.shift();
+        console.log(penumpang.join(' '));
+        // Mengembalikan dan keluar
+        
+    }
+}
