0f52605df0560b7ba079d8356cae1e3600116282
Faiz-Super <faizmhmad15@gmail.com>
2025-08-22 08:29:41 +0700

    Implementasi Function & Array

--- Project func & arr/project.js
+++ Project func & arr/project.js
@@ -1,11 +1,36 @@
-var penumpang = [];
+var penumpang = ['Faiz', undefined, 'Ilham'];
 var tambahPenumpang = function(namaPenumpang, penumpang) {
 
-    if (penumpang == 0){
-        // Menambah penumpang
-        penumpang.shift();
-        console.log(penumpang.join(' '));
+    // Jika angkot kosong
+    if (penumpang.length == 0){
+        // Menambah penumpang di awal array
+        penumpang.push(namaPenumpang);
         // Mengembalikan dan keluar
-        
+        return penumpang;
+    } else {
+        // Telurusuri seluruh kursi awal
+        for ( var i = 0; i < penumpang.length; i++){
+            // Jika ada kursi kosong
+            if ( penumpang[i] == undefined){
+                // Tambah penumpang dikursi tersebut
+                penumpang[i] = namaPenumpang;
+                // Kembalikan isi array dan keluar dari function
+                return penumpang;
+
+                // Jika ada nama penumpang yang sama
+            } else if (penumpang[i] == namaPenumpang){
+                // tampilkan pesan kesalahan
+                console.log(namaPenumpang + ' Sudah di dalam angkot!');
+                // kembalikan isi array dan keluar dari function
+                return penumpang;
+
+                // Jika seluruh kursi sudah terisi
+            } else if ( i == penumpang.length - 1){
+                // tambah penumpang di akhir array
+                penumpang.push(namaPenumpang);
+                // kembalikan isi array dan keluar dari function
+                return penumpang;
+            }
+        }
     }
 }
